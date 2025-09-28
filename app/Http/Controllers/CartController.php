2803c2941ba3e50ee0813a9d6277e283d6ccd8e4
Daniel Tavares <danieltvs33@gmail.com>
2025-09-28 18:37:44 +0100

    feat :: create cart table and some functionality

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\AddProductToCart;
+use App\Models\Cart;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/add-product', [HomeController::class, 'addProduct'])->name('addProduct');
 Route::post('/delete-product/{product}', [HomeController::class, 'deleteProduct'])->name('deleteProduct');
+Route::post('/add-product-to-cart', [HomeController::class, 'addProductToCart'])->name('addProductToCart');
 
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
